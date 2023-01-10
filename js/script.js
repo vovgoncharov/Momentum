fee547b9d05a10556e68b65b5c2f0112c0ff7547
Vova <deykdog@gmail.com>
2023-01-10 11:55:52 +0200

    fix: plaer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,61 @@
 import { getData } from "./data.js";
-import { playMusic } from "./audio.js";
 import { getGreet } from "./greeting.js";
 import { nextSlide, prevSlide } from "./slider.js";
 import { getWeather } from "./weather.js";
+
+const play = document.querySelector(".play");
+const next = document.querySelector(".play-next");
+const prev = document.querySelector(".play-prev");
+const playList = document.querySelector(".play-list");
+const list = document.querySelector(".list");
+const audio = new Audio();
+const musics = [
+    "Aqua Caelestis",
+    "Ennio Morricone",
+    "River Flows In You",
+    "Summer Wind",
+];
+const playMusic = () => {
+    let zero = 0;
+    play.addEventListener("click", (e) => {
+        if (document.querySelector(".play.pause")) {
+            audio.currentTime = 0;
+            audio.pause();
+            playList.innerHTML = `${musics[zero]}`;
+            play.classList.remove("pause");
+            list.classList.remove("display");
+        } else {
+            list.classList.add("display");
+            audio.src = `../assets/sounds/${musics[zero]}.mp3`;
+            audio.currentTime = 0;
+            audio.play();
+            playList.innerHTML = `${musics[zero]}`;
+            play.classList.add("pause");
+        }
+    });
+
+    next.addEventListener("click", (e) => {
+        list.classList.add("display");
+        if (zero <= musics.length - 2) {
+            zero += 1;
+            audio.src = `../assets/sounds/${musics[zero]}.mp3`;
+            audio.currentTime = 0;
+            audio.play();
+            playList.innerHTML = `${musics[zero]}`;
+            play.classList.add("pause");
+        }
+    });
+
+    prev.addEventListener("click", (e) => {
+        list.classList.add("display");
+        if (zero > 0) {
+            zero -= 1;
+            audio.src = `../assets/sounds/${musics[zero]}.mp3`;
+            audio.currentTime = 0;
+            audio.play();
+            playList.innerHTML = `${musics[zero]}`;
+            play.classList.add("pause");
+        }
+    });
+};
+playMusic();
