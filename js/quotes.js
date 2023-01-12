6ae7993407b33049a13b8229b7449882f0c97833
Vova <deykdog@gmail.com>
2023-01-12 14:35:42 +0200

    add: module

--- js/quotes.js
+++ js/quotes.js
@@ -1,7 +1,22 @@
-import { data } from "./data.js";
+
 const quote = document.querySelector(".quote");
 const author = document.querySelector(".author");
 const changeQuote = document.querySelector(".change-quote");
+
+ const data =[
+    {
+      "text": "Пишите код так, как будто сопровождать его будет склонный к насилию психопат, который знает, где вы живете",
+      "author": "Стив Макконнелл"
+    },
+    {
+      "text": "Сложность программы растет до тех пор, пока не превысит способности программиста",
+      "author": "Артур Блох. Законы Мэрфи"
+    },
+      {
+      "text": "Ходить по воде и разрабатывать программы, следуя ТЗ, очень просто… если они заморожены",
+      "author": "И. Берард"
+    }
+  ]  
 
 export const getQuote = () => {
     data.forEach(() => {
--- js/script.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-import { getData } from "./date.js";
-import { getGreet } from "./greeting.js";
-import { nextSlide, prevSlide } from "./slider.js";
-import { getWeather } from "./weather.js";
-import { playMusic } from "./audio.js";
-import { getQuote } from "./quotes.js";
